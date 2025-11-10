23bddcc544f4115a93c0ba1a9899a94b1f0a6087
Poyraz <poyrazg19@hotmai.com>
2025-11-10 15:59:49 -0600

    Added category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_10_214146) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_10_215707) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
