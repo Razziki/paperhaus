5301a0cca2e51fff45716eebe4e4ef3e0ab0c91c
Poyraz <poyrazg19@hotmai.com>
2025-11-10 16:04:16 -0600

    Migrated products

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_10_215707) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_10_220017) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.text "description"
+    t.integer "price_cents"
+    t.string "currency"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +38,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_10_215707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "products", "categories"
 end
