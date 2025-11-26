40c79407bcc424c7ea0b6af80896beba0ada4a03
Poyraz <poyrazg19@hotmai.com>
2025-11-26 14:34:55 -0600

    added search and dropdown for category search and pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,20 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product
-                  .includes(:category)
-                  .order(:name)
-                  .page(params[:page])
-                  .per(3)
-  end
+    @search       = params[:search].to_s.strip
+    @category_id  = params[:category_id].presence
 
-  def show
-    @product = Product.includes(:category).find(params[:id])
+    scope = Product.includes(:category).order(:name)
+
+    if @search.present?
+      query = "%#{@search}%"
+      scope = scope.where("products.name LIKE ? OR products.description LIKE ?", query, query)
+    end
+
+    if @category_id
+      scope = scope.where(category_id: @category_id)
+    end
+
+    @products = scope.page(params[:page]).per(10)
+    @categories = Category.order(:name)
   end
 end
