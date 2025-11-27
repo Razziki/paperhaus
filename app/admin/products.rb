1937888f5642b3dd1ba0547db45174810f834d07
Poyraz <poyrazg19@hotmai.com>
2025-11-27 16:09:11 -0600

    removed image url column since using active storage

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,10 +1,23 @@
 # app/admin/products.rb
 ActiveAdmin.register Product do
-  permit_params :name, :author, :description, :price_cents, :currency, :category_id
+  permit_params :name, :author, :description, :price_cents, :currency, :category_id, :image
 
   filter :name
   filter :author
   filter :category
 
   remove_filter :order_items
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :author
+      f.input :description
+      f.input :price_cents
+      f.input :currency
+      f.input :category
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
-
+  has_one_attached :image
   validates :name, presence: true
   validates :price_cents, numericality: { greater_than_or_equal_to: 0 }
 
