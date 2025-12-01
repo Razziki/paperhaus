02b79fc332f56eeb367f12025f62687f4179e787
Poyraz <poyrazg19@hotmai.com>
2025-12-01 15:46:19 -0600

    Added admin navbar link, improve About/Contact pages, and use user email in ActiveAdmin user dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
+
+    def to_s
+    email   # or "#{first_name} #{last_name} (#{email})"
+  end
 end
