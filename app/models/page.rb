05795966cbe7f26d82c32317e8438ed89ce2b247
Poyraz <poyrazg19@hotmai.com>
2025-12-01 15:01:31 -0600

    allowed ransack searching/sorting on these fields

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,13 @@ class Page < ApplicationRecord
   validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
   validates :body, presence: true
+
+  #allow ransack searching/sorting on these fields
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id title slug body created_at updated_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
